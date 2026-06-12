1bea1c99bbb35297f77660afbea60db11fdb3064
moyu100100-png <moyu100100@gmail.com>
2026-06-12 19:43:06 +0900

    add capacitor ios

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+ 
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'My Wishlist',
+  webDir: 'out',
+};
+
+export default config;
--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  // suppress turbopack root warning
-  experimental: {},
+  output: 'export',
 };
 
 export default nextConfig;
